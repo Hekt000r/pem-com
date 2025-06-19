2de80471ba68fd9ee4a1dbfa04647ab7253fab3b
Hektor <hektorzaimi.dev@gmail.com>
2025-06-19 17:05:13 +0200

    more work on admin page

--- src/Components/CompanyAdminNavbar.tsx
+++ src/Components/CompanyAdminNavbar.tsx
@@ -1,25 +1,100 @@
 import "./components.css";
-import {Montserrat} from `next/font/google`
+import { useState } from "react";
+import { IoPersonCircle } from "react-icons/io5";
+import { FaGear, FaBookmark, FaDoorOpen } from "react-icons/fa6";
+import Image from "next/image";
+import { signOut, useSession } from "next-auth/react";
+
 interface Company {
-    name: string,
-    displayName: string,
-    imgURL: string
+  name: string;
+  displayName: string;
+  imgURL: string;
 }
 interface CompanyAdminNavbarProps {
-    imgURL: string;
-    company: Company;
+  imgURL: string;
+  company: Company;
 }
 
-export default function CompanyAdminNavbar({imgURL, company}: CompanyAdminNavbarProps) {
+export default function CompanyAdminNavbar({
+  imgURL,
+  company,
+}: CompanyAdminNavbarProps) {
+  const [isVisible, setIsVisible] = useState(false);
+  const { data: session } = useSession();
 
-    return (
+  return (
     <>
-    <div className="h-14 flex shadow-xl">
-        <div className="flex p-1.5 ml-1 h-14 items-center">
-            <img className="rounded-md w-10 h-10" src={imgURL} alt="" />
-            <h1 className="ml-2 font-montserrat">{company.displayName} Admin</h1>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap"
+        rel="stylesheet"
+      ></link>
+      <div className="h-16 flex shadow-xl">
+        <div className="flex p-1.5 ml-1 h-16 items-center">
+          <img
+            className="rounded-md w-12 h-12 cursor-pointer hover:brightness-80 transition"
+            src={imgURL}
+            alt=""
+          />
+          <h1 className="ml-2 font-montserrat font-semibold">
+            {company.displayName} Admin
+          </h1>
         </div>
-    </div>
+        <div className="flex flex-1 justify-end items-end mr-8">
+          {session ? (
+            <div className="flex relative items-center justify-center h-full">
+              <button className="h-10 btn btn-ghost p-1 flex ml-2 mr-2 items-center justify-center">
+                <h1 className="justify-center h-12 flex items-center mr-2">
+                  <IoPersonCircle className="m-2 w-6 h-6 text-xl" /> Dil prej Admin
+                </h1>
+              </button>
+
+              <button
+                onClick={() => setIsVisible((prev) => !prev)}
+                className="w-10 h-10 btn btn-ghost rounded-full overflow-hidden p-0.5 flex items-center justify-center"
+              >
+                <Image
+                  width={40}
+                  height={40}
+                  className="w-full h-full object-cover rounded-full"
+                  src={session?.user?.image!}
+                  alt="User"
+                />
+              </button>
+
+              <div
+                className={`absolute top-12 right-0 shadow-xl border border-black rounded-2xl h-40 w-56 bg-white flex flex-col items-end z-50 transition-all duration-200 ease-out
+                            ${
+                              isVisible
+                                ? "opacity-100 scale-100 pointer-events-auto"
+                                : "opacity-0 scale-95 pointer-events-none"
+                            }
+                            `}
+                style={{ transformOrigin: "top right" }}
+              >
+                <button className="w-[90%] btn btn-ghost rounded-xl p-1 mt-2 h-10 justify-start mr-2 pr-2 flex items-center">
+                  <FaGear className="w-5 h-5" />
+                  <h1 className="text-md">Settings</h1>
+                </button>
+                <button className="w-[90%] btn btn-ghost rounded-xl p-1 mt-2 h-10 justify-start mr-2 pr-2 flex items-center">
+                  <FaBookmark className="w-5 h-5" />
+                  <h1 className="text-md">Punët e ruajtura</h1>
+                </button>
+                <button
+                  onClick={() => {
+                    signOut();
+                  }}
+                  className="w-[90%] btn btn-error rounded-xl p-1 mt-2 h-10 justify-center mr-2 pr-2 flex items-center"
+                >
+                  <FaDoorOpen className="w-5 h-5" />
+                  <h1 className="text-md">Dil</h1>
+                </button>
+              </div>
+            </div>
+          ) : null}
+        </div>
+
+        
+      </div>
     </>
-  )
+  );
 }
--- src/app/pem-admin/page.tsx
+++ src/app/pem-admin/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import "@/Components/components.css"
+"use client";
+import "@/Components/components.css";
 import CompanyAdminNavbar from "@/Components/CompanyAdminNavbar";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -8,47 +8,91 @@ import Loading from "@/Components/Loading";
 import axios from "axios";
 
 interface Company {
-    name: string,
-    displayName: string,
-    imgURL: string
+  name: string;
+  displayName: string;
+  imgURL: string;
 }
 
 export default function Page() {
-    const [loading, setLoading] = useState(true);
-    const [company, setCompany] = useState<Company>()
-    const router = useRouter();
-    const {data: session} = useSession()
-    useEffect(() => {
-        const checkAdmin = async () => {
-            if (!session || !session.user || !session.user.oauthId) {
-                setLoading(false);
-                router.replace("/");
-                return;
-            }
-            try {
-                const res = await axios.get(`/api/getUserAdminCompany?oid=${session.user.oauthId}`);
-
-                setCompany(res.data)
-            } catch (e) {
-                router.replace("/");
-                console.log(`error: ${e}`)
-            } finally {
-                setLoading(false);
-            }
-        };
-        if (session) {
-            checkAdmin();
-        }
-    }, [session]);
-
-    if (loading) {
-        return (
-            <Loading/>
+  const [loading, setLoading] = useState(true);
+  const [company, setCompany] = useState<Company>();
+  const router = useRouter();
+  const { data: session } = useSession();
+  useEffect(() => {
+    const checkAdmin = async () => {
+      if (!session || !session.user || !session.user.oauthId) {
+        setLoading(false);
+        router.replace("/");
+        return;
+      }
+      try {
+        const res = await axios.get(
+          `/api/getUserAdminCompany?oid=${session.user.oauthId}`
         );
+
+        setCompany(res.data);
+      } catch (e) {
+        router.replace("/");
+        console.log(`error: ${e}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (session) {
+      checkAdmin();
     }
-    return (
+  }, [session]);
+
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      <CompanyAdminNavbar company={company!} imgURL={company?.imgURL!} />
+      <div className="p-4 m-4">
         <div>
-            <CompanyAdminNavbar company={company!} imgURL={company?.imgURL!}/>
+          <h1 className="font-montserrat text-3xl font-semibold">
+            Përshëndetje, {session?.user.name} !
+          </h1>
+          <h2 className="text-gray-700 text-lg mt-2">
+            Menaxho kompaninë, krijo postime, ose bisedo me klientët.
+          </h2>
         </div>
-    )
+
+        <div className="flex justify-center items-center mt-8 space-x-12 flex-wrap">
+          <div className="shadow-md h-44 w-80 mt-2 border-[1px] border-gray-300 rounded-2xl ">
+            <div className="flex m-3">
+              <img
+                src={company?.imgURL}
+                className="w-12 hover:brightness-80 cursor-pointer h-12 rounded-md"
+                alt=""
+              />
+              <div className="flex-col ml-2">
+                <h1 className="font-montserrat font-semibold">
+                  {company?.displayName}
+                </h1>
+                <h2 className="font-montserrat">Plani: Biznes i vogël</h2>
+              </div>
+            </div>
+            <div className="m-2 p-1 font-montserrat">
+              <div>
+                <h2>
+                  Postimet: <b>11</b> nga <b>15</b>
+                </h2>
+                <h2>
+                  Adminët: <b>1</b> nga <b>5</b>
+                </h2>
+              </div>
+              <div className="flex justify-end items-end h-full">
+                <button className="btn btn-primary btn-sm">Ndrysho planin</button>
+              </div>
+            </div>
+          </div>
+          <div className="shadow-md h-44 w-80 mt-2 border-[1px] border-gray-300 rounded-2xl "></div>
+          <div className="shadow-md h-44 w-80 mt-2 border-[1px] border-gray-300 rounded-2xl "></div>
+          <div className="shadow-md h-44 w-80 mt-2 border-[1px] border-gray-300 rounded-2xl "></div>
+        </div>
+      </div>
+    </div>
+  );
 }
