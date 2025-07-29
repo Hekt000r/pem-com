06d651045e440007226cbb6ebe82579fc5bbf282
Hektor <hektorzaimi.dev@gmail.com>
2025-07-29 15:56:36 +0200

    [Frontend] Improve home page for non-logged in users and changes to /login/page

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,14 +1,18 @@
 "use client";
 import { useState } from "react";
 import { FaMapMarkerAlt } from "react-icons/fa";
-import { FaCity } from "react-icons/fa6";
+import { FaCity, FaShieldHalved } from "react-icons/fa6";
+import { FiZap } from "react-icons/fi";
+import { IoChatbubble, IoChatbubbleOutline } from "react-icons/io5";
+import { MdOutlineSupportAgent } from "react-icons/md";
+import { URL } from "url";
 
 export default function Hero() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchCity, setSearchCity] = useState("");
 
   return (
-    <div className="bg-linear-to-r from-green-500 to-emerald-600 w-screen h-80">
+    <div className="bg-linear-to-r from-green-500 to-emerald-600 w-full h-80">
       <div className="flex flex-col justify-center items-center h-[100%]">
         <h1 className="text-white font-montserrat font-semibold text-4xl">
           Gjej punën e ëndrrave
@@ -66,6 +70,119 @@ export default function Hero() {
             >
               Kërko
             </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex items-center flex-col">
+        <h1 className="font-montserrat text-4xl font-bold mt-8">
+          Pse të na zgjidhni ne?
+        </h1>
+
+        <div className="flex mt-8 space-x-16">
+          <div className="flex justify-center flex-col items-center">
+            <div className="rounded-full bg-green-200 w-16 h-16 flex items-center justify-center">
+              <IoChatbubbleOutline className="w-10 h-10 stroke-emerald-600" />
+            </div>
+            <h1 className="mt-2 font-montserrat font-semibold text-xl">
+              Komunikim Direkt
+            </h1>
+
+            <p className="max-w-64 text-center text-gray-600 font-medium mt-2">
+              Bisedo direkt me punëdhënësit pa ndërmjetës. Merr përgjigje të
+              shpejta dhe të qarta.
+            </p>
+          </div>
+
+          <div className="flex justify-center flex-col items-center">
+            <div className="rounded-full bg-green-200 w-16 h-16 flex items-center justify-center">
+              <FiZap className="w-10 h-10 stroke-emerald-600" />
+            </div>
+            <h1 className="mt-2 font-montserrat font-semibold text-xl">
+              {" "}
+              Shpejtësi
+            </h1>
+
+            <p className="max-w-64 text-center text-gray-600 font-medium mt-2">
+              Apliko për punë në vetëm disa klikime. Procesi ynë i thjeshtë të
+              kursen kohë të çmuar.
+            </p>
+          </div>
+          <div className="flex justify-center flex-col items-center">
+            <div className="rounded-full bg-green-200 w-16 h-16 flex items-center justify-center">
+              <FaShieldHalved className="w-10 h-10 text-emerald-600" />
+            </div>
+            <h1 className="mt-2 font-montserrat font-semibold text-xl">
+              Siguria e plotë
+            </h1>
+
+            <p className="max-w-64 text-center text-gray-600 font-medium mt-2">
+              Të dhënat tuaja janë të sigurta. Ne verifikojmë të gjitha
+              kompanitë për sigurinë tuaj.
+            </p>
+          </div>
+          <div className="flex justify-center flex-col items-center">
+            <div className="rounded-full bg-green-200 w-16 h-16 flex items-center justify-center">
+              <MdOutlineSupportAgent className="w-10 h-10 text-emerald-600" />
+            </div>
+            <h1 className="mt-2 font-montserrat font-semibold text-xl">
+              Mbështetje Profesionale
+            </h1>
+
+            <p className="max-w-64 text-center text-gray-600 font-medium mt-2">
+              Merrni mbështetje të shpejtë dhe efikase nga ekipi ynë i dedikuar
+              për të gjitha nevojat tuaja.z
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-col flex items-center mb-24">
+          <h1 className="text-4xl font-bold mt-8">Si Funksionon?</h1>
+          <h2 className="mt-2 text-2xl font-semibold text-gray-600">
+            Vetëm 3 hapa drejt punës së re
+          </h2>
+
+          <div className="flex flex-wrap gap-x-12">
+            <div className="flex flex-col items-center mt-8">
+              <div className="flex items-center justify-center rounded-full bg-emerald-600 w-20 h-20">
+                <h1 className="text-white font-bold text-3xl">1</h1>
+              </div>
+              <h1 className="mt-2 text-xl font-montserrat font-semibold">
+                Regjistrohu Falas
+              </h1>
+
+              <h2 className="max-w-72 font-montserrat font-medium text-center mt-1">
+                Krijo profilin tënd në vetëm disa minuta. Shto përvojën,
+                aftësitë dhe preferencat e tua.
+              </h2>
+            </div>
+
+            <div className="flex flex-col items-center mt-8">
+              <div className="flex items-center justify-center rounded-full bg-emerald-600 w-20 h-20">
+                <h1 className="text-white font-bold text-3xl">2</h1>
+              </div>
+              <h1 className="mt-2 text-xl font-montserrat font-semibold">
+                Kërko & Apliko
+              </h1>
+
+              <h2 className="max-w-72 font-montserrat font-medium text-center mt-1">
+                Shfletoni mijëra punë dhe aplikoni për ato që ju përshtaten.
+                Kërkoni sipas vendndodhjes dhe rrogës.
+              </h2>
+            </div>
+
+            <div className="flex flex-col items-center mt-8">
+              <div className="flex items-center justify-center rounded-full bg-emerald-600 w-20 h-20">
+                <h1 className="text-white font-bold text-3xl">3</h1>
+              </div>
+              <h1 className="mt-2 text-xl font-montserrat font-semibold">
+                Bisedo & Fillo
+              </h1>
+
+              <h2 className="max-w-72 font-montserrat font-medium text-center mt-1">
+                Komuniko direkt me punëdhënësit, bëj intervistën, dhe filloni
+                një Punë të mbarë!
+              </h2>
+            </div>
           </div>
         </div>
       </div>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,21 +4,65 @@ import { FaGoogle } from "react-icons/fa";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
 import GoogleSignInButton from "@/Components/GoogleSignInButton";
+import { FaBuilding, FaBuildingUser } from "react-icons/fa6";
 
 export default function Login() {
   const { data: session } = useSession();
 
   return (
     <SessionProvider>
-      {session ? redirect("/home") : (
+      {session ? (
+        redirect("/home")
+      ) : (
         <div className="flex justify-center items-center h-screen bg-slate-100">
           <div className="shadow-2xl w-[30%] h-[50%] rounded-2xl bg-white border-black">
             <div className="m-4 mt-8 flex flex-col items-center justify-center">
-              <a href="/"><img src="Logo1.svg" className="w-52" /></a>
-              <p className="mt-2">Përshëndetje! Identifikohuni ose krijoni një llogari të re.</p>
-              <span className="h-0.5 mt-2 bg-gray-300 shadow-2xl w-[80%]"></span>
+              <a href="/">
+                <img src="Logo1.svg" className="w-52" />
+              </a>
+              <p className="mt-2">
+                Përshëndetje! Identifikohuni ose krijoni një llogari të re.
+              </p>
+              <span className="h-0.25 mt-2 bg-gray-300 shadow-2xl w-[80%]"></span>
 
-              <div className="mt-2"><GoogleSignInButton onClick={() => signIn("google")} size="large"/></div>
+              <div className="mt-2">
+                <GoogleSignInButton
+                  onClick={() => signIn("google")}
+                  size="large"
+                />
+              </div>
+
+              <div className="w-full flex items-center justify-center space-x-4 mt-4">
+                <span className="h-px bg-gray-300 flex-1"></span>
+                <h1 className="text-gray-500 font-medium">ose</h1>
+                <span className="h-px bg-gray-300 flex-1"></span>
+              </div>
+
+              <button
+                className="h-12 px-6 text-base
+        bg-white 
+        btn
+        border-gray-300 
+        text-gray-700 
+        font-medium 
+        hover:bg-gray-50 
+        hover:border-gray-400
+        focus:bg-gray-50
+        disabled:opacity-50 
+        disabled:cursor-not-allowed
+        transition-all 
+        duration-200
+        flex 
+        items-center 
+        justify-center 
+        gap-3
+        min-w-fit
+        mt-2
+        "
+              >
+                <FaBuildingUser className="w-6 h-6"/>
+                Regjistro një kompani
+              </button>
             </div>
           </div>
         </div>
