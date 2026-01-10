825b128f67e7970fc10fb99bb98b2950398b61e1
Hektor <hektorzaimi.dev@gmail.com>
2026-01-10 15:32:00 +0100

    [Frontend] Begin work on remaking superadmin page

---
+++ src/app/pem-admin/company-applications/page.tsx
@@ -0,0 +1,8 @@
+
+export default function CompanyApplications() {
+    return (
+        <div className="w-full h-full bg-red-500">
+            <h1>Aplikimet e kompanive</h1>
+        </div>
+    )
+}   
--- src/app/pem-admin/layout.tsx
+++ src/app/pem-admin/layout.tsx
@@ -3,8 +3,9 @@ import Loading from "@/Components/Loading";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { FaBook } from "react-icons/fa6";
+import { FaBook, FaClipboardList } from "react-icons/fa6";
 import { IoDocumentText, IoHome } from "react-icons/io5";
+import { FaHome } from "react-icons/fa";
 
 export default function SuperAdminLayout({ children }: any) {
   const [loading, setLoading] = useState(true);
@@ -24,52 +25,56 @@ export default function SuperAdminLayout({ children }: any) {
     });
   }, []);
 
-  const activeBtnClass = `h-14 btn btn-ghost flex text-blue-700 bg-blue-100 font-bold`;
-  const inactiveBtnClass = `h-14 btn btn-ghost flex`;
+  const activeBtnClass = `btn btn-ghost border bg-blue-50 border-blue-400 shadow-sm rounded-sm flex justify-start font-legacy-montserrat text-blue-700 items-center w-full`;
+  const inactiveBtnClass = `btn btn-ghost border border-gray-400 shadow-sm rounded-sm flex justify-start font-legacy-montserrat text-gray-700 items-center w-full`;
   const isActive = (page: string) => currentPath === page;
 
   if (loading) return <Loading />;
 
   return (
-    <div className="h-screen w-72 border-gray-300 border-1 shadow-2xl">
-      <div className="m-4 flex flex-col space-y-2">
-        <a
-          className={`items-center border-gray-300 border-2 justify-center w-64 cursor-pointer space-x-1 ${
-            isActive("/pem-admin/home") ? activeBtnClass : inactiveBtnClass
-          }`}
-        >
-          <IoHome className="w-6 h-6" />{" "}
-          <h1 className="text-lg font-semibold font-legacy-montserrat">
-            Ballina
-          </h1>
-        </a>
-
-        <a
-          className={`items-center border-gray-300 border-2 justify-center w-64 cursor-pointer space-x-1 ${
-            isActive("/pem-admin/company-applications")
-              ? activeBtnClass
-              : inactiveBtnClass
-          }`}
-        >
-          <IoDocumentText className="w-6 h-6" />{" "}
-          <h1 className="text-md max-w-48 font-semibold font-legacy-montserrat overflow-visible">
-            Aplikimet e kompanive
-          </h1>
-        </a>
-
-        <a
-          className={`items-center border-gray-300 border-2 justify-center w-64 cursor-pointer space-x-1 ${
-            isActive("/pem-admin/company-applications")
-              ? activeBtnClass
-              : inactiveBtnClass
-          }`}
-        >
-          <FaBook className="w-5 h-5" />{" "}
-          <h1 className="text-md max-w-48 font-semibold font-legacy-montserrat overflow-visible">
-            Regjistri i veprimeve
-          </h1>
-        </a>
+    <div className="flex">
+      <div className="h-screen w-64 border-gray-300 border-1 shadow-2xl p-3 pt-6 space-y-2">
+        <div>
+          <a
+            href="/pem-admin/home"
+            className={
+              isActive("/pem-admin/home") ? activeBtnClass : inactiveBtnClass
+            }
+          >
+            <FaHome className={`w-5 h-5`} /> Ballina
+          </a>
+        </div>
+        <div className="">
+          <p className="font-legacy-montserrat text-gray-900   text-sm font-medium">
+            Kompanitë
+          </p>
+          <span className="divider my-0"></span>
+        </div>
+        <div>
+          <a
+            href="/pem-admin/company-applications"
+            className={
+              isActive("/pem-admin/company-applications")
+                ? activeBtnClass
+                : inactiveBtnClass
+            }
+          >
+            <FaClipboardList className={`w-5 h-5`} /> Aplikimet e kompanive
+          </a>
+        </div>
+        <div>
+          <a
+            className={
+              isActive("/pem-admin/company-logs")
+                ? activeBtnClass
+                : inactiveBtnClass
+            }
+          >
+            <FaBook className={`w-5 h-5`} /> Regjistri i veprimeve
+          </a>
+        </div>
       </div>
+      {children}
     </div>
   );
 }
