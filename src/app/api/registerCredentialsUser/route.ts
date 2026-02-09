d831cc4ac676606edc0e7cc4d204dcf6a150bdbd
Hektor <Hekt000r@users.noreply.github.com>
2026-02-09 14:55:21 +0100

    [Backend] Uploading user avatars to Supabase S3 as public images upon creation
    Part of issue https://github.com/Hekt000r/pem-com/issues/10
    
    Instead of just linking to images on third-party sites, when a user is registered, now they will have their avatar uploaded to Supabase.
    
    Changes:
    - uploadPublicImage API has been removed, since that approach was a bad way to begin with
    - Google OAuth2 authentication flow has been changed to upload user's Google avatar before finishing registration
    - registerCredentialsUser has been changed to upload a placeholder avatar to supabase, which can then get replaced when the profile is finished (this requires frontend connection and the finishing of finishProfile)
    - Updated next.config.ts to include new supabase S3 storage URL's

--- src/app/api/registerCredentialsUser/route.ts
+++ src/app/api/registerCredentialsUser/route.ts
@@ -1,4 +1,5 @@
 import { connectToDatabase } from "@/utils/mongodb";
+import { UploadPublicImage } from "@/utils/supabase/uploadPublicImage";
 import bcrypt from "bcryptjs";
 import { nanoid } from "nanoid";
 import { NextRequest, NextResponse } from "next/server";
@@ -16,40 +17,56 @@ export async function POST(req: NextRequest) {
   if (existingUser) {
     return NextResponse.json(
       { error: "Email already in use" },
-      { status: 409 }
+      { status: 409 },
     );
   }
 
-  // Attempt insert (catch duplicate insert caused by race condition)
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
+    const userOAuthID = nanoid();
+
+    // Upload user avatar to Supabase
+    let ImageURL =
+      "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"; // Wikipedia Fallback
+
+    const response = await fetch(ImageURL);
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const storagePath = `avatars/${userOAuthID}`;
+
+    const supabaseUrl = await UploadPublicImage(
+      buffer,
+      storagePath,
+      response.headers.get("content-type") || "image/png",
+    );
+
+    ImageURL = supabaseUrl
 
     const user = {
       email,
       password: hashedPassword,
-      oauthId: nanoid(),
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png",
+      oauthId: userOAuthID,
+      image: ImageURL,
       hasProfile: false,
     };
 
     await EndusersCollection.insertOne(user);
 
     return NextResponse.json({ success: true }, { status: 201 });
-
   } catch (err: any) {
     // MongoDB duplicate key error (email unique index)
     if (err.code === 11000) {
       return NextResponse.json(
         { error: "Email already in use" },
-        { status: 409 }
+        { status: 409 },
       );
     }
 
     console.error("Registration error:", err);
     return NextResponse.json(
       { error: "Internal server error" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
